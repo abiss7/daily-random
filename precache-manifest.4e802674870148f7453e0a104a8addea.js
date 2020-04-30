18d03b33598dfb3738d22d8470a601f53c44d7a2
vates_abiss <vates_abiss@lnexternos.com.ar>
2020-04-30 09:55:37 -0300

    Updates

---
+++ precache-manifest.4e802674870148f7453e0a104a8addea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb309101dd127f778e7b7dda4cad8f3d",
+    "url": "/daily-random/index.html"
+  },
+  {
+    "revision": "9623752422dda3746b77",
+    "url": "/daily-random/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "521d9ba8cde97d8827ce",
+    "url": "/daily-random/static/css/main.345e0b86.chunk.css"
+  },
+  {
+    "revision": "9623752422dda3746b77",
+    "url": "/daily-random/static/js/2.57c82321.chunk.js"
+  },
+  {
+    "revision": "cfc125643707176b9979a69345bf80d5",
+    "url": "/daily-random/static/js/2.57c82321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521d9ba8cde97d8827ce",
+    "url": "/daily-random/static/js/main.87303481.chunk.js"
+  },
+  {
+    "revision": "a1c2536f76709a489d89",
+    "url": "/daily-random/static/js/runtime-main.eeff36aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daily-random/static/media/logo.5d5d9eef.svg"
+  }
+]);
