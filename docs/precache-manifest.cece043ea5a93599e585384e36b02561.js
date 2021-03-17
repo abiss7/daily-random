e7863da13f17da4309256da8953d7709f0c5053b
Biss Andres (Consultora Vates) <vates_abiss@lnexternos.com.ar>
2021-03-16 21:04:54 -0300

    docs

---
+++ docs/precache-manifest.cece043ea5a93599e585384e36b02561.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a459fa41546d6686f65f9839f22a36",
+    "url": "/daily-random/index.html"
+  },
+  {
+    "revision": "5e11f7327666f349eca5",
+    "url": "/daily-random/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d4e336373c50b17326a8",
+    "url": "/daily-random/static/css/main.1639fedd.chunk.css"
+  },
+  {
+    "revision": "5e11f7327666f349eca5",
+    "url": "/daily-random/static/js/2.c52380f2.chunk.js"
+  },
+  {
+    "revision": "cfc125643707176b9979a69345bf80d5",
+    "url": "/daily-random/static/js/2.c52380f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e336373c50b17326a8",
+    "url": "/daily-random/static/js/main.e073562f.chunk.js"
+  },
+  {
+    "revision": "a1c2536f76709a489d89",
+    "url": "/daily-random/static/js/runtime-main.eeff36aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daily-random/static/media/logo.5d5d9eef.svg"
+  }
+]);
